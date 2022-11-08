cd55769678664ef99a33bd2ca86bbd0400e447fa
sj1705 <shresthajaiswal1705@gmail.com>
2022-11-08 12:50:01 +0530

    initial

---
+++ linearsearch/__init__.py
@@ -0,0 +1,16 @@
+import time
+
+
+class LinearSearch:
+
+    def __init__(self, data, search):
+        self.data = data
+        self.search = int(search)
+
+    def linear_search(self, draw_data, time_tick):
+        for i in range(len(self.data)):
+            draw_data(self.data, ['blue' if x == i else 'green' for x in range(len(self.data))])
+            if self.data[i] == self.search:
+                return
+            time.sleep(time_tick)
+        draw_data(self.data, ['red' for _ in range(len(self.data))])
